63ca29aae81fb94491322bb1a23103cdd508ecc3
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-01-06 22:01:54 +0530

    Route is and controller is created

---
+++ server/controllers/userController.js
@@ -0,0 +1,54 @@
+const User = require('../models/userModel')
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+    res.status(200).json(users)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
+const addUser = async (req, res) => {
+  try {
+    const { name, age, location } = req.body
+    const user = await User.create({ name, age: Number(age), location})
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { name, age, location } = req.body
+    const user = await User.findOneAndUpdate({ _id: id }, { name, age: Number(age), location}, { new: true })
+    if (!user) {
+      return res.status(404).json({ error: 'The User doesnt exist' })
+    }
+    res.status(200).json({ _id: id, name, age, location })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params
+    const del = await User.findOneAndDelete({ _id: id })
+    if (!del) {
+      return res.status(404).json({ error: 'The User does not exists' })
+    }
+    res.status(200).json({ deleted: true })
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+
+module.exports = {
+  getUsers,
+  addUser,
+  updateUser,
+  deleteUser
+}
